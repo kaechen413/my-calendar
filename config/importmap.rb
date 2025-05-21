1e6ebb52fcf1dbd6f6d5900326e6deefa1dcb615
Jiahui Chen <cjh1171740108@gmail.com>
2025-05-21 10:26:23 +0900

    backup

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,3 +7,9 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "bootstrap", to: "bootstrap.min.js", preload: true
 pin "@popperjs/core", to: "popper.js", preload: true
+pin "fullcalendar" # @6.1.17
+pin "@fullcalendar/core", to: "@fullcalendar--core.js" # @6.1.17
+pin "preact" # @10.12.1
+pin "preact/compat", to: "preact--compat.js" # @10.12.1
+pin "preact/hooks", to: "preact--hooks.js" # @10.12.1
+pin "@fullcalendar/daygrid", to: "@fullcalendar--daygrid.js" # @6.1.17
